acb6b940006bda38b5dfe4b59500cfc3f978fd5a
DianaM411 <dianamunteanu220@gmail.com>
2022-04-25 17:16:59 +0200

    clean

--- src/com/diana/Main.java
+++ src/com/diana/Main.java
@@ -5,49 +5,26 @@ package com.diana;
         comunes (y cuántas veces aparecen). Prueba el programa con los archivos de la carpeta ‘Libros’.*/
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-    public static HashMap<String, Integer> cuentaPalabras(String texto) throws FileNotFoundException {
-        String saltador = " .,;\n\r\t";
-        HashMap<String, Integer> letras = new HashMap<String, Integer>();
-        java.util.Scanner teclado = new java.util.Scanner(new File(texto));
-        String p;
-
-        for (; teclado.hasNextLine();) {
-
-            StringTokenizer linea = new StringTokenizer(teclado.nextLine(), saltador);
-
-            for (; linea.hasMoreTokens();) {
-
-                p = linea.nextToken();
-
-                if (!letras.containsKey(p)) {
-                    letras.put(p, 1);
-
-                } else {
-                    letras.put(p, letras.get(p) + 1);
-                }
-            }
-        }
-        return letras;
-    }
 
     public static void main(String[] args) {
         Scanner sc = null;
-
         try {
-        //el archivo que queremos leer
-        File archivo = new File("C:\\Users\\diana\\OneDrive\\Desktop\\learnProgramming\\B7\\Documentos\\Libros\\lazarillo.txt");
-        sc = new Scanner(archivo);
-        //declaramos variables
-        int palabras = 0;
-        int lineas = 0;
-        int caracteres = 0;
-        String p;
-        //Creamos un objeto de tipo HashMap
-        Map<String, Integer> letras = new HashMap<String,Integer>();
+            //el archivo que queremos leer
+            String rutaRelativa = "Documentos\\frases.txt";
+            String pwd = System.getProperty("user.dir");//ruta directorio actual de trabajo
+            String rutaAbsoluta = pwd + File.separator + rutaRelativa;
+            File archivo = new File(rutaAbsoluta);
+            sc = new Scanner(archivo);
+            //declaramos variables
+            int palabras = 0;
+            int lineas = 0;
+            int caracteres = 0;
+            String p;
+            //Creamos un objeto de tipo HashMap
+            Map<String, Integer> letras = new HashMap<String, Integer>();
 
 
             while (sc.hasNextLine()) {//mientras hay mas lineas del archivo para leer
@@ -59,7 +36,7 @@ public class Main {
 
                 StringTokenizer linea = new StringTokenizer(sc.nextLine(), saltador);
 
-                for (; linea.hasMoreTokens();) {
+                for (; linea.hasMoreTokens(); ) {
 
                     p = linea.nextToken();
 
@@ -72,37 +49,25 @@ public class Main {
                 }
             }
 
-          /*  SortedMap map = new TreeMap(java.util.Collections.reverseOrder());
+            SortedMap map = new TreeMap(java.util.Collections.reverseOrder());
             map.putAll(letras);
             // Lee el TreeMap y te muestra los resultados en orden Descendente
             Iterator iterator = map.keySet().iterator();
-            while (iterator.hasNext()) {
+            int i =1;
+            System.out.println(" ");
+            System.out.println("las 10 palabras más comunes y cuántas veces aparecen: ");
+            System.out.println(" ");
+            while (i <= 10) {
                 Object key = iterator.next();
                 System.out.println("Clave : " + key + " Valor :" + map.get(key));
-            }*/
-
-
-
-           //ValueComparator bvc =  new ValueComparator(letras);
-          SortedMap map = new TreeMap(java.util.Collections.reverseOrder());
-           // Map<String, Integer> map = new TreeMap<>();
-            map.putAll(letras);
-            System.out.println("After Sorting: ");
-            Set set2 = map.entrySet();
-            Iterator iterate2 = set2.iterator();
-            while(iterate2.hasNext())
-            {
-                Map.Entry me2 = (Map.Entry)iterate2.next();
-                System.out.print(me2.getValue() + ": ");
-                System.out.println(me2.getKey());
+                i++;
             }
 
-            //imprimimos numero de lenies, palabras y caracteres
+            //imprimimos numero de lineas, palabras y caracteres
+            System.out.println(" ");
             System.out.println("Numero de lineas: " + lineas);
             System.out.println("Numero de palabras: " + palabras);
             System.out.println("Numero de caracteres: " + caracteres);
-
-           // System.out.println(cuentaPalabras("C:\\Users\\diana\\OneDrive\\Desktop\\learnProgramming\\B7\\Documentos\\Libros\\lazarillo.txt"));
 
         } catch (Exception e) {  //manejamos excepciones
             e.printStackTrace();
